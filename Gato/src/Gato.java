64b6849375820c819eea70bb1a9c2b7d205a62a5
Carlos Fernando Elizalde Carrillo <124462478+fernando1410@users.noreply.github.com>
2023-03-08 21:32:18 -0600

    Add files via upload

---
+++ Gato/src/Gato.java
@@ -0,0 +1,15 @@
+public class Gato {
+    public static void main(String[] args) {
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+        System.out.println("      │      │      ");
+    }
+}
