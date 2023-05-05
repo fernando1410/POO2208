b82dad0eb9b2890b403780591faa1b5cc77b0968
Carlos Fernando Elizalde Carrillo <124462478+fernando1410@users.noreply.github.com>
2023-05-04 21:54:32 -0600

    Add files via upload

---
+++ VentanaSwing/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaSwing VentanaSwing = new VentanaSwing("Centígrados a Fahrenheit");
+    }
+}
