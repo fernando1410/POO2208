ff1659a7342da931aaefaae68570b52bf96f9572
Carlos Fernando Elizalde Carrillo <124462478+fernando1410@users.noreply.github.com>
2023-05-27 00:15:28 -0600

    Add files via upload
    Actualizado

--- SQLiteDemo2/src/main/java/org/example/Main.java
+++ SQLiteDemo2/src/main/java/org/example/Main.java
@@ -1,27 +1,15 @@
 package org.example;
 
+import org.example.controlador.ControladorLibro;
 import org.example.modelo.Libro;
-import org.example.persistencia.DemoLibroDB;
 import org.example.persistencia.LibroDAO;
+import org.example.vista.VentanaLibro;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        DemoLibroDB demo = new DemoLibroDB();
-        LibroDAO ldao = new LibroDAO();
-
-       try {
-           Libro res = (Libro) ldao.buscarPorId("1");
-           System.out.println(res);
-           System.out.println("-----------------------");
-           for (Object lib:ldao.obtenerTodo()) {
-               System.out.println((Libro)lib);
-           }
-
-       }catch (SQLException sqle){
-           System.out.println("Error al eliminar");
-       }
+        VentanaLibro view = new VentanaLibro("Ejemplo");
+        ControladorLibro controller = new ControladorLibro(view);
     }
 }
